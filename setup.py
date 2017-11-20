e4ee03e637023ae89e85b8b3a678cdfcaccd5688
Mohit Saini <mohitsaini@Mohits-MacBook-Pro-2.local>
2017-11-20 15:27:02 +0530

    First commit for msl3

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+	name='msl',
+	version="1.0.0",
+	url='https://github.com/mohitmv/msl3',
+	author='Mohit Saini',
+	author_email='mohitsaini1196@gmail.com',
+	description='Simple extension of python inbuilt functions',
+	long_description='Simple extension of python inbuilt functions',
+	packages=['msl'],
+	install_requires=[],
+	extras_require={}, 
+	classifiers=[
+		'Programming Language :: Python :: 3',
+	],
+	entry_points={}
+)
+
